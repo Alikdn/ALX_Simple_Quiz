692eae0b021c42c95d962ba795a1edd5f5fd55e7
ALI Khaldoun <ali.khaldoun03@gmail.com>
2024-07-14 23:17:48 +0200

    commit changes

--- quiz.js
+++ quiz.js
@@ -1,25 +1,20 @@
 // quiz.js
 function checkAnswer() {
     // The correct answer
-    const correctAnswer = "4";
+    const correctAnswer = "4"; 
     
     selected = document.querySelector("input[name='quiz']:checked");
     const userAnswer = selected.value;
     
-    // Check if an answer was selected
-    if (!userAnswer) {
-        document.getElementById('feedback').textContent = "Please select an answer.";
-        return;
-    }
-    
     // Compare the user's answer with the correct answer
-    if (userAnswer.value === correctAnswer) {
+    if (userAnswer === correctAnswer) {
         document.getElementById('feedback').textContent = "Correct! Well done.";
-    } else {
+    } 
+    else {
         document.getElementById('feedback').textContent = "That's incorrect. Try again!";
     }
 }
 
-// Add event listener to the submit button
+// event listener to the submit button
 const submitButton = document.getElementById('submit-answer');
 submitButton.addEventListener('click', checkAnswer);
